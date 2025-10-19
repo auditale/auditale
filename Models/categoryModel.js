834ba048a31a1a67c118a4523003747822693b95
auditale <app.auditale@gmail.com>
2025-10-19 16:07:58 -0400

    Keyur - change the model from category->genre

--- Models/categoryModel.js
+++ Models/categoryModel.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
-    categoryName:{
+const genreSchema = new mongoose.Schema({
+    genreName:{
         type: String,
         required: true,
         unique: true
     },
-    categoryDesc:{
+    genreDesc:{
         type: String,
         required: true,
     }
 }, { timestamps: true });
 
-const Category = mongoose.model('Category', categorySchema);
-module.exports = Category;
+const genre = mongoose.model('genre', genreSchema);
+module.exports = genre;
